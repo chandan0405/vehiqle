c2190a3af5f3c7b77e141ac9cc6dff0df2a93f97
Chandan.Kumar <chandan04051999@gmail.com>
2025-04-13 12:51:57 +0530

    Set up the Prisma, Created the database schema ,and migration too.

---
+++ src/lib/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+/* eslint-disable no-var */
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+/* eslint-enable no-var */
+
+let prisma: PrismaClient;
+
+if (isProduction) {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export const db = prisma;
